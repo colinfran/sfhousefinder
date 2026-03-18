a894b6114fe032740859f09299098b9f108feb7d
Colin Franceschini <hello@colinfran.com>
2026-03-17 18:44:34 -0700

    Update header.tsx

--- dashboard-app/components/header.tsx
+++ dashboard-app/components/header.tsx
@@ -7,7 +7,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 const Header = (): JSX.Element => {
   return (
     <header className="sticky top-0 z-30 flex h-[50px] items-center justify-between gap-4 border-b bg-background px-4 sm:static sm:border-0 sm:bg-transparent sm:px-6">
-      <Link className="flex w-full items-center gap-3" href="/">
+      <Link className="flex items-center gap-3" href="/">
         <div className="flex h-8 w-8 items-center justify-center rounded-md border border-border bg-card">
           <Home className="h-4 w-4" />
         </div>
